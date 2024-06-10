cda4d5142008718dd36f6a9bc5c45ddf29c04cc5
geok108 <geokleanthous94@gmail.com>
2024-06-10 22:45:42 +0300

    updated gitignore

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ teamsDict = {
 	"Othellos": "Οθέλλος",
 	"AEL": "ΑΕΛ"
 }
-currentRound = footballData.getCurrentRound(True)
+currentRound = footballData.getCurrentRound()
 print(currentRound)
 
 currentRoundFixtures = footballData.getCurrentRoundFixtures(currentRound)
@@ -42,29 +42,18 @@ for fixture in currentRoundFixtures["response"]:
 	fixtureDate = date_object.strftime("%d/%m/%Y")
 	
 	lastFiveGamesFormHomeTeam = footballData.getTeamStats(fixture["teams"]["home"]["id"])["response"]["form"][-5:]
-	lastFiveGamesFormAwayTeam = footballData.getTeamStats(fixture["teams"]["away"]["id"])["response"]["form"][-5:]
-	# print("---HOME TEAM STATS---")
-	
+	lastFiveGamesFormAwayTeam = footballData.getTeamStats(fixture["teams"]["away"]["id"])["response"]["form"][-5:]	
 
 	homeTeamGoalsForInHome = footballData.getTeamStats(fixture["teams"]["home"]["id"])["response"]["goals"]["for"]["average"]["home"]
 	homeTeamGoalsForAway = footballData.getTeamStats(fixture["teams"]["home"]["id"])["response"]["goals"]["for"]["average"]["away"]
 	homeTeamGoalsAgainstInHome = footballData.getTeamStats(fixture["teams"]["home"]["id"])["response"]["goals"]["against"]["average"]["home"]
 	homeTeamGoalsAgainstAway = footballData.getTeamStats(fixture["teams"]["home"]["id"])["response"]["goals"]["against"]["average"]["away"]
-	# print("homeTeam goals for in home " + homeTeamGoalsForInHome)
-	# print("homeTeam goals for away " + homeTeamGoalsForAway)
-	# print("homeTeam goals against in home " + homeTeamGoalsAgainstInHome)
-	# print("homeTeam goals against in away " + homeTeamGoalsAgainstAway)
 
-	# print("---AWAY TEAM STATS---")
 	awayTeamGoalsForInHome = footballData.getTeamStats(fixture["teams"]["away"]["id"])["response"]["goals"]["for"]["average"]["home"]
 	awayTeamGoalsForAway = footballData.getTeamStats(fixture["teams"]["away"]["id"])["response"]["goals"]["for"]["average"]["away"]
 	awayTeamGoalsAgainstInHome = footballData.getTeamStats(fixture["teams"]["away"]["id"])["response"]["goals"]["against"]["average"]["home"]
 	awayTeamGoalsAgainstAway = footballData.getTeamStats(fixture["teams"]["away"]["id"])["response"]["goals"]["against"]["average"]["away"]
-	# print("awayTeam goals for in home " + awayTeamGoalsForInHome)
-	# print("awayTeam goals for away " + awayTeamGoalsForAway)
-	# print("awayTeam goals against in home " + awayTeamGoalsAgainstInHome)
-	# print("awayTeam goals against in away " + awayTeamGoalsAgainstAway)
-
+	
 	h2h = footballData.getHeadToHead(fixture["teams"]["home"]["id"], fixture["teams"]["away"]["id"])
 	homeTeamWins = 0
 	awayTeamWins = 0
@@ -105,18 +94,18 @@ for fixture in currentRoundFixtures["response"]:
 	}
 	
 	for key, value in placeholders.items():
-  	  promptTemplate = re.sub(key, value, promptTemplate)
+		promptTemplate = re.sub(key, value, promptTemplate)
 
-	# promptTemplate.replace("{homeTeam}", fixture["teams"]["home"]["name"])
-	# promptTemplate.replace("{awayTeam}", fixture["teams"]["away"]["name"])
-	# promptTemplate.replace("{head2head}", fixture["teams"]["home"]["name"] + " wins: " 
-	# 							 + str(homeTeamWins) + ", " + fixture["teams"]["away"]["name"] + " wins: " + str(awayTeamWins)
-	# 							 + ", draws: " + str(drawsCount))	
-	# promptTemplate.replace("{homeTeamForm}", lastFiveGamesFormHomeTeam)
-	# promptTemplate.replace("{awayTeamForm}", lastFiveGamesFormAwayTeam)
+		promptTemplate.replace("{homeTeam}", fixture["teams"]["home"]["name"])
+		promptTemplate.replace("{awayTeam}", fixture["teams"]["away"]["name"])
+		promptTemplate.replace("{head2head}", fixture["teams"]["home"]["name"] + " wins: " 
+									+ str(homeTeamWins) + ", " + fixture["teams"]["away"]["name"] + " wins: " + str(awayTeamWins)
+									+ ", draws: " + str(drawsCount))	
+		promptTemplate.replace("{homeTeamForm}", lastFiveGamesFormHomeTeam)
+		promptTemplate.replace("{awayTeamForm}", lastFiveGamesFormAwayTeam)
 
 	print("CHAT GPT PROMPT: " + promptTemplate)
-	break
+	# break
 	# chatgpt endpoint call
 	print("GENERATING ARTICLE...")
 	completion = client.chat.completions.create(
